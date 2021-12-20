9453c64f827b417611b14a2d72bcf5d2e014378e
TerryluvsRoblox <terrynguyen965@gmail.com>
2021-12-20 21:45:19 +0700

    move highlighting

--- highlighting.py
+++ highlighting.py
@@ -2,19 +2,20 @@ import pygame as pyg
 
 pyg.init()
 
-BLUE = pyg.Color('blue')
-YELLOW = pyg.Color('yellow')
+BLUE = pyg.Color("blue")
+YELLOW = pyg.Color("yellow")
+RED = (255, 68, 68)
 
 
 def highlightSq(screen, gs, validMoves, sq_selected, size):
     if sq_selected != ():
         r, c = sq_selected
-        if gs.board[r, c][0] == ('w' if gs.whitesturn else 'b'):
+        if gs.board[r, c][0] == ("w" if gs.whitesturn else "b"):
             s = pyg.Surface((size, size))
             s.set_alpha(100)
-            s.fill(BLUE)
-            screen.blit(s, (c*size, r*size))
-            s.fill(YELLOW)
+            s.fill(RED)
+            screen.blit(s, (c * size, r * size))
+            s.fill(RED)
             for move in validMoves:
                 if move.startRow == r and move.startCol == c:
-                    screen.blit(s, (move.endCol*size, move.endRow*size))
+                    screen.blit(s, (move.endCol * size, move.endRow * size))
