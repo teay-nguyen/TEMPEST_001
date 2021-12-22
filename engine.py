600f0afb2598b323e6c62dfa26bed8e3c9009165
TerryluvsRoblox <terrynguyen965@gmail.com>
2021-12-22 22:45:29 +0700

    testing some stuff

--- engine.py
+++ engine.py
@@ -81,7 +81,7 @@ class State:
     def fenToPos(self):
         self.init_board_pieces = []
         fen = 'rnbq1k1r/pp1Pbppp/2p5/8/2B5/8/PPP1NnPP/RNBQK2R w KQ - 1 8'
-        #fen = 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1'
+        fen = 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1'
         self.board = np.array(
             [
                 ["--", "--", "--", "--", "--", "--", "--", "--"],
