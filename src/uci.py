15889198c2fa2e35284b271062b4611537f694c7
TerryluvsRoblox <terrynguyen965@gmail.com>
2022-05-15 13:30:59 +0700

    parse move

--- src/uci.py
+++ src/uci.py
@@ -17,3 +17,30 @@
     along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 '''
+
+# imports
+import search
+import board0x88
+import evaluation
+from defs import *
+
+# parse move string and turn it into something the engine can understand
+def parse_move(move_str:str, pos:board0x88.BoardState):
+    move_list:board0x88.MovesStruct = board0x88.MovesStruct()
+    pos.gen_moves(move_list)
+    parse_from:int = (ord(move_str[0]) - ord('a')) + (8 - (ord(move_str[1]) - ord('0'))) * 16
+    parse_to:int = (ord(move_str[2]) - ord('a')) + (8 - (ord(move_str[3]) - ord('0'))) * 16
+    promoted_piece:int = 0
+    move:int = 0
+    for c in range(move_list.count):
+        move:int = move_list.moves[c].move
+        if get_move_source(move) == parse_from and get_move_target(move) == parse_to:
+            promoted_piece:int = get_move_promoted(move)
+            if promoted_piece:
+                if (promoted_piece == N or promoted_piece == n) and move_str[4] == 'n': return move
+                elif (promoted_piece == B or promoted_piece == b) and move_str[4] == 'b': return move
+                elif (promoted_piece == R or promoted_piece == r) and move_str[4] == 'r': return move
+                elif (promoted_piece == Q or promoted_piece == q) and move_str[4] == 'q': return move
+                continue
+            return move
+    return 0
