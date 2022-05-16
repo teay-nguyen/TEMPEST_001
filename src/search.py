248bd6949ca70b8689f79dc13712866289a49d42
TerryluvsRoblox <terrynguyen965@gmail.com>
2022-05-16 21:47:24 +0700

    more accurate print

--- src/search.py
+++ src/search.py
@@ -52,8 +52,9 @@ mvv_lva:list = [
 POS_INF:int = int(1e8)
 NEG_INF:int = int(-1e8)
 MATE_VAL:int = 50000
+MATE_SCORE:int = 49000
 MAX_PLY:int = 64
-OPTIMAL_DEPTH:int = 5
+OPTIMAL_DEPTH:int = 6
 TIME_LIMIT_FOR_SEARCH:int = 30
 
 tt = Transposition()
@@ -148,7 +149,9 @@ class _standard():
             if not elapsed: elapsed = 1
             if self.timing_util['abort']: break
             if self.pv_length[0]:
-                print(f'  info depth {c_d} nodes {self.nodes} score {score} nps {get_ms(self.nodes)//elapsed} tbhits {self.tb_hits} time {elapsed} pv', end=' ')
+                if score > -MATE_VAL and score < -MATE_SCORE: print(f'info depth {c_d} nodes {self.nodes} mate {-(score+MATE_VAL)//2-1} nps {get_ms(self.nodes)//elapsed} tbhits {self.tb_hits} time {elapsed} pv', end=' ')
+                elif score > MATE_SCORE and score < MATE_VAL: print(f'info depth {c_d} nodes {self.nodes} mate {(MATE_VAL-score)//2+1} nps {get_ms(self.nodes)//elapsed} tbhits {self.tb_hits} time {elapsed} pv', end=' ')
+                else: print(f'info depth {c_d} nodes {self.nodes} score {score} nps {get_ms(self.nodes)//elapsed} tbhits {self.tb_hits} time {elapsed} pv', end=' ')
                 for _m in range(self.pv_length[0]): print_move(self.pv_table[0][_m])
                 if score <= -MATE_VAL: self.enabled = 0; print('     IS MATED! | GAMEOVER!\n', end=''); return 0
                 elif score <= -MATE_VAL + MAX_PLY: print('     GETTING MATED!\n', end=''); break
@@ -158,7 +161,7 @@ class _standard():
                     if not _v or _v == K or _v == k: continue
                     if pos.pce_count[_v]: absolute_draw = 0
                 if absolute_draw: self.enabled = 0; print('\n  GAME DRAWN!'); break
-            else: print(f'  info depth {c_d} nodes {self.nodes} score {score} nps {get_ms(self.nodes)//elapsed} time {elapsed}     IS MATED! | GAMEOVER!\n', end=' '); self.enabled = 0; return 0
+            else: print(f'info depth 0 mate 0     IS MATED! | GAMEOVER!\n', end=' '); self.enabled = 0; return 0
         print('\n', end='  ')
         if self.pv_table[0][0]:
             print('bestmove', end=' ')
